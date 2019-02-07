1efa2ea9dcd82f24eea4734550fcb9ac57366ae6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-07 14:32:08 +0000

    build based on e6095ae

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "SCF.jl",
+    "title": "SCF.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#SCF.jl-1",
+    "page": "SCF.jl",
+    "title": "SCF.jl",
+    "category": "section",
+    "text": "A Julia library for the self-consistent field method, mainly intended to solve problems on the formbeginequation\nE(vecPvecc) = fracvecc^H\nmatHveccvecc^Hvecc\nendequationwhere vecP is a set of orbitals, vecc is a vector of mixing coefficients, andbeginequation\nmatH_ij defd matrixelP_iHamiltonianP_j\nendequationSuch equations arise in the multi-configurational (Dirac–)Hartree–Fock approximation, used in theoretical atomic and molecular physics.This library only implements the SCF procedure. The actual equations that are to be solved have to be setup by the user (for atoms, the Atoms.jl library may be used)."
+},
+
+{
+    "location": "#References-1",
+    "page": "SCF.jl",
+    "title": "References",
+    "category": "section",
+    "text": "Fischer, C. F. (1986). Self-Consistent-Field (SCF) and Multiconfiguration (MC) Hartree-Fock (HF) Methods in Atomic Calculations: Numerical Integration Approaches. Computer Physics Reports, 3(5), 274–325. DOI: 10.1016/0167-7977(86)90001-8"
+},
+
+]}
