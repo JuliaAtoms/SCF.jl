fed975080087e9e9884cbef801cd480b3d5fd33a
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-04-12 13:24:47 +0200

    Moved to Github Actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,26 @@
-using Documenter
 using SCF
+using Documenter
 
-makedocs(
-    sitename = "SCF",
-    modules = [SCF],
-    pages = [
+makedocs(;
+    modules=[SCF],
+    authors="Stefanos Carlström <stefanos.carlstrom@gmail.com>",
+    repo="https://github.com/JuliaAtoms/SCF.jl/blob/{commit}{path}#{line}",
+    sitename="SCF.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="juliaatoms.org",
+        assets=String["assets/latex.js"],
+        mathengine = Documenter.MathJax()
+    ),
+    pages=[
         "Home" => "index.md",
         "Quantum Systems" => "quantum_systems.md",
         "Fock Operators" => "fock_operators.md",
         "Self-Consistent Iteration" => "self_consistent_iteration.md"
     ],
-    assets = ["assets/latex.js"],
+    doctest=false,
 )
 
-deploydocs(repo = "github.com/JuliaAtoms/SCF.jl.git")
+deploydocs(;
+    repo="github.com/JuliaAtoms/SCF.jl",
+)
